82051713b153bb90ed1e5360efc58fdd9bb7b2a0
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-04-17 19:00:37 +0530

    test drpdwn added

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function oncli()
 {
 
 }
+
+// If you want the dropdown to disappear when you move the mouse away
+var dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(function(dropdown) {
+  dropdown.addEventListener('mouseleave', function() {
+    dropdown.querySelector('.dropdown-menu').style.display = 'none';
+  });
+});
